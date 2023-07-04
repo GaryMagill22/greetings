639b074662d77264177b8c1a0ff51c91d3bfcb8f
Gary Magill <Garymagill22@gmail.com>
2023-07-04 15:56:15 -0600

    First Java Test Program

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My name is Gary Magill");
+        System.out.println("I am 32 years old!");
+        System.out.println("My hometown is Malvern, Pennsylvania");
+    }
+}
